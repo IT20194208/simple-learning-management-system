8551b89bc07a4b96bc7cdd2fdd2345a714bfe103
Ranasinghe R.P.C.P <73824269+IT20194208@users.noreply.github.com>
2023-08-30 20:37:13 +0530

    i created lms backend application
    only structure added

---
+++ lms-backend/src/test/java/com/lmsbackend/lmsbackend/LmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmsbackend.lmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
