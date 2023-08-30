4e7dabe865d9f5959eff0273520d52dad2e87bda
Ranasinghe R.P.C.P <73824269+IT20194208@users.noreply.github.com>
2023-08-30 21:45:26 +0530

    create entity class
    create entity class name is student entity class or actually it is the table representation

---
+++ lms-backend/src/main/java/com/lmsbackend/lmsbackend/entity/StudentEntity.java
@@ -0,0 +1,40 @@
+package com.lmsbackend.lmsbackend.entity;
+
+import com.vladmihalcea.hibernate.type.json.JsonType;
+import lombok.*;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "student")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@TypeDefs({
+        @TypeDef(name = "json",typeClass = JsonType.class)
+})
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "sid",length = 40)
+    private int sid;
+
+    @Column(name = "name",length = 50,nullable = false)
+    private String name;
+
+    @Column(name = "address",length = 200,nullable = false)
+    private String address;
+
+    @Type(type = "json")
+    @Column(name = "tel",length = 15,nullable = false,columnDefinition = "json")
+    private ArrayList tel;
+
+    @Column(name = "nic",length = 15,nullable = false)
+    private String nic;
+}
